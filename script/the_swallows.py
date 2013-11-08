c6224ae7b026ac4384f4672a751da52b42f8a727
Cat's Eye Technologies <cpressey@gmail.com>
2013-11-08 18:58:16 +0000

    Package all the outputting stuff into a LegacyPublisher object.

--- script/the_swallows.py
+++ script/the_swallows.py
@@ -12,11 +12,9 @@ import sys
 sys.path.insert(0, join(dirname(realpath(sys.argv[0])), '..', 'src'))
 
 # now we can:
-from swallows.world import alice, bob, house
-from swallows.util import pick
-from swallows.events import EventCollector, Editor
+from swallows.events import LegacyPublisher
 
-# this is not good.  but it works.
+# the following is not good.  but it works.
 # better options would be:
 # - define the world-specific behaviour of the characters in swallows.world
 # - (less better) have alice & bob take these objects as dependency injections
@@ -28,98 +26,10 @@ swallows.objects.dead_body = swallows.world.dead_body
 
 ### main ###
 
-FRIFFERY = False
-DEBUG = False
-
-print "Title TBD (Book Four of _The Swallows_ series)"
-print "=============================================="
-print
-
-for chapter in range(1, 17):
-    print "Chapter %d." % chapter
-    print "-----------"
-    print
-
-    alice_collector = EventCollector()
-    bob_collector = EventCollector()
-    # don't continue a conversation from the previous chapter, please
-    alice.topic = None
-    bob.topic = None
-    alice.location = None
-    bob.location = None
-
-    for paragraph in range(1, 26):
-        alice.collector = alice_collector
-        bob.collector = bob_collector
-        
-        # we could do this randomly...
-        #pov_actor = pick([alice, bob])
-        # but, we could also alternate.  They ARE Alice and Bob, after all.
-        pov_actor = (alice, bob)[(paragraph - 1) % 2]
-
-        for actor in (alice, bob):
-            if actor.location is None:
-                actor.place_in(pick(house))
-            else:
-                # this is hacky & won't work for >2 characters:
-                if not (alice.location == bob.location):
-                    actor.emit("<1> was in <2>", [actor, actor.location])
-
-        actor_order = (alice, bob)
-        # this leads to continuity problems:
-        #if random.randint(0, 1) == 0:
-        #    actor_order = (bob, alice)
-        while len(pov_actor.collector.events) < 20:
-            for actor in actor_order:
-                actor.live()
-
-        if FRIFFERY:
-            if paragraph == 1:
-                choice = random.randint(0, 3)
-                if choice == 0:
-                    sys.stdout.write("It was raining.  ")
-                if choice == 1:
-                    sys.stdout.write("It was snowing.  ")
-                if choice == 2:
-                    sys.stdout.write("The sun was shining.  ")
-                if choice == 3:
-                    sys.stdout.write("The day was overcast and humid.  ")
-            elif not str(c.events[0]).startswith("'"):
-                choice = random.randint(0, 8)
-                if choice == 0:
-                    sys.stdout.write("Later on, ")
-                if choice == 1:
-                    sys.stdout.write("Suddenly, ")
-                if choice == 2:
-                    sys.stdout.write("After a moment's consideration, ")
-                if choice == 3:
-                    sys.stdout.write("Feeling anxious, ")
-
-        if DEBUG:
-            print "ALICE'S POV:"
-            for event in alice_collector.events:
-                print str(event)
-            print
-            alice.dump_memory()
-            print
-            print "BOB'S POV:"
-            for event in bob_collector.events:
-                print str(event)
-            print
-            bob.dump_memory()
-            print
-            print "- - - - -"
-            print
-
-        if not DEBUG:
-            editor = Editor()
-            for event in pov_actor.collector.events:
-                editor.read(event)
-            for event in editor.events:
-                sys.stdout.write(str(event) + "  ")
-                #sys.stdout.write("\n")
-            print
-            print
-
-        alice_collector.events = []
-        bob_collector.events = []
+publisher = LegacyPublisher(
+    alice=swallows.world.alice,
+    bob=swallows.world.bob,
+    house=swallows.world.house,
+    title="Title TBD (Book Four of _The Swallows_ series)",
+)
+publisher.publish()
--- src/swallows/events.py
+++ src/swallows/events.py
@@ -92,7 +92,7 @@ oblivion = Oblivion()
 #   "Bob went to the shed.  Bob saw Alice." into
 #   "Bob went to the shed, where he saw Alice."
 # btw, we currently get a new editor for every paragraph
-class Editor(object):
+class LegacyEditor(object):
     """The Editor is remarkably similar to the _peephole optimizer_ in
     compiler construction.  Instead of replacing sequences of instructions
     with more efficient but semantically equivalent sequences of
@@ -108,10 +108,10 @@ class Editor(object):
         self.events = []
 
     def read(self, event):
-        if len(self.events) < Editor.MEMORY:
+        if len(self.events) < self.__class__.MEMORY:
             self.events.append(event)
             return
-        
+
         character = event.participants[0]
         # update our idea of their location
         self.character_location[character.name] = character.location
@@ -142,3 +142,119 @@ class Editor(object):
         else:  # new character doing stuff
             self.character = character
             self.events.append(event)
+
+
+class LegacyPublisher(object):
+    """Publisher which uses the old Event/Editor framework.
+
+    Will probably go away eventually, but nice to have as a reference
+    while working on the new Event/Editor framework.
+
+    """
+    def __init__(self, **kwargs):
+        self.alice = kwargs.get('alice')
+        self.bob = kwargs.get('bob')
+        self.house = kwargs.get('house')
+        self.friffery = kwargs.get('friffery', False)
+        self.debug = kwargs.get('debug', False)
+        self.title = kwargs.get('title', "Untitled")
+        self.chapters = kwargs.get('chapters', 16)
+        self.paragraphs_per_chapter = kwargs.get('paragraphs_per_chapter', 25)
+
+    def publish(self):
+        print self.title
+        print "=" * len(self.title)
+        print
+
+        alice = self.alice
+        bob = self.bob
+        house = self.house
+
+        for chapter in range(1, self.chapters+1):
+            print "Chapter %d." % chapter
+            print "-----------"
+            print
+        
+            alice_collector = EventCollector()
+            bob_collector = EventCollector()
+            # don't continue a conversation from the previous chapter, please
+            alice.topic = None
+            bob.topic = None
+            alice.location = None
+            bob.location = None
+        
+            for paragraph in range(1, self.paragraphs_per_chapter+1):
+                alice.collector = alice_collector
+                bob.collector = bob_collector
+                
+                # we could do this randomly...
+                #pov_actor = pick([alice, bob])
+                # but, we could also alternate.  They ARE Alice and Bob, after all.
+                pov_actor = (alice, bob)[(paragraph - 1) % 2]
+        
+                for actor in (alice, bob):
+                    if actor.location is None:
+                        actor.place_in(pick(house))
+                    else:
+                        # this is hacky & won't work for >2 characters:
+                        if not (alice.location == bob.location):
+                            actor.emit("<1> was in <2>", [actor, actor.location])
+        
+                actor_order = (alice, bob)
+                # this leads to continuity problems:
+                #if random.randint(0, 1) == 0:
+                #    actor_order = (bob, alice)
+                while len(pov_actor.collector.events) < 20:
+                    for actor in actor_order:
+                        actor.live()
+        
+                if self.friffery:
+                    if paragraph == 1:
+                        choice = random.randint(0, 3)
+                        if choice == 0:
+                            sys.stdout.write("It was raining.  ")
+                        if choice == 1:
+                            sys.stdout.write("It was snowing.  ")
+                        if choice == 2:
+                            sys.stdout.write("The sun was shining.  ")
+                        if choice == 3:
+                            sys.stdout.write("The day was overcast and humid.  ")
+                    elif not str(c.events[0]).startswith("'"):
+                        choice = random.randint(0, 8)
+                        if choice == 0:
+                            sys.stdout.write("Later on, ")
+                        if choice == 1:
+                            sys.stdout.write("Suddenly, ")
+                        if choice == 2:
+                            sys.stdout.write("After a moment's consideration, ")
+                        if choice == 3:
+                            sys.stdout.write("Feeling anxious, ")
+        
+                if self.debug:
+                    print "ALICE'S POV:"
+                    for event in alice_collector.events:
+                        print str(event)
+                    print
+                    alice.dump_memory()
+                    print
+                    print "BOB'S POV:"
+                    for event in bob_collector.events:
+                        print str(event)
+                    print
+                    bob.dump_memory()
+                    print
+                    print "- - - - -"
+                    print
+        
+                if not self.debug:
+                    editor = LegacyEditor()
+                    for event in pov_actor.collector.events:
+                        editor.read(event)
+                    for event in editor.events:
+                        sys.stdout.write(str(event) + "  ")
+                        #sys.stdout.write("\n")
+                    print
+                    print
+        
+                alice_collector.events = []
+                bob_collector.events = []
